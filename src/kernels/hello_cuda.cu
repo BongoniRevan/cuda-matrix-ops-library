f361834d6d9aa37f74b28a518a4abc82cab50329
BongoniRevan <rbongoni2-c@my.cityu.edu.hk>
2026-05-23 09:08:30 +0000

    Phase 1: environment setup, repo structure, hello CUDA vector addition

---
+++ src/kernels/hello_cuda.cu
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+__global__ void addVectors(float* a, float* b, float* c, int n) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n) {
+        c[i] = a[i] + b[i];
+    }
+}
+
+int main() {
+    int n = 1024;
+    size_t size = n * sizeof(float);
+
+    float *h_a, *h_b, *h_c;
+    h_a = (float*)malloc(size);
+    h_b = (float*)malloc(size);
+    h_c = (float*)malloc(size);
+
+    for (int i = 0; i < n; i++) {
+        h_a[i] = 1.0f;
+        h_b[i] = 2.0f;
+    }
+
+    float *d_a, *d_b, *d_c;
+    cudaMalloc(&d_a, size);
+    cudaMalloc(&d_b, size);
+    cudaMalloc(&d_c, size);
+
+    cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, size, cudaMemcpyHostToDevice);
+
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (n + threadsPerBlock - 1) / threadsPerBlock;
+    addVectors<<<blocksPerGrid, threadsPerBlock>>>(d_a, d_b, d_c, n);
+
+    cudaMemcpy(h_c, d_c, size, cudaMemcpyDeviceToHost);
+
+    printf("c[0] = %f (expected 3.0)\n", h_c[0]);
+    printf("c[1023] = %f (expected 3.0)\n", h_c[1023]);
+
+    cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+    free(h_a); free(h_b); free(h_c);
+    return 0;
+}
